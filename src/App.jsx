c4b3cdf5be3a01315042df3cf4970f3644cc652e
Jorge Miranda <jmiranda32691@gmail.com>
2026-02-02 17:07:30 -0600

    finished hw. Struggled a little with show selection

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
+import {tvShows} from './shows/data'
+import React from "react"; 
+import ShowSelection from "./shows/ShowSelection";
+import ShowDetails from "./shows/ShowDetails";
+
+
 /**
  * React TV is an web streaming platform that allows users to browse
  * through the episodes of a variety of different shows.
  */
 export default function App() {
+
+  const [selectedShow, setSelectedShow] = React.useState();
+
+  
+ 
+  
   return (
     <>
       <header>
         <p>React TV</p>
+        <ShowSelection 
+        shows={tvShows} 
+        selectedShow={selectedShow} 
+        setSelectedShow={setSelectedShow}/>
       </header>
-      <main></main>
+      <main>
+        <ShowDetails key={selectedShow?.name} show={selectedShow}/>
+      </main>
     </>
   );
 }
